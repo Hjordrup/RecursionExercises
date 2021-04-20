41baf3952616a1382687f89d1a731b67d41cdc82
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-04-20 16:10:28 +0200

    Add a RecursiveBinarySearch

---
+++ src/RecursiveBinarySearch.java
@@ -0,0 +1,34 @@
+public class RecursiveBinarySearch {
+
+
+    public static void main(String[] args) {
+
+        int[] list = {1,2,3,4,5,6,7,8,9,100};
+        int index = recursiveBinarySearch(list, 12);
+        System.out.println( index == -1 ? "The element is not in the list ": "The element is placed in the "
+        + index +" index ");
+
+    }
+
+    public static int recursiveBinarySearch(int[] list, int key){
+        int low = 0;
+        int high = list.length-1;
+        return recursiveBinarySearch(list, key, low, high);
+    }
+
+    public static int recursiveBinarySearch(int[] list, int key, int low, int high){
+        if(low>high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(key == list[mid] ){
+            return mid;
+        }
+        if(key<list[mid]){
+            return recursiveBinarySearch(list, key, low, mid -1 );
+        }else return recursiveBinarySearch(list, key, mid +1, high);
+    }
+
+
+
+}
